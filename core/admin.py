bb014b86137fec716eaba6aca3d33bcaf9d70af7
Lostyzz <lucaslostyz@gmail.com>
2025-12-19 23:24:35 -0300

    Criação models Campeão

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Campeao
 # Register your models here.
+
+admin.site.register(Campeao)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Campeao(models.Model):
+    nome = models.CharField(
+        max_length = 100,
+        verbose_name = 'Nome do campeão',
+    )
+
+    funcao = models.CharField(
+        verbose_name = 'Função do campeão',
+        max_length = 100
+    )
+
+    descricao = models.TextField(
+        verbose_name = 'Descrição do campeão'
+    )
+
+    class Meta:
+        verbose_name = 'Campeão',
+        verbose_name_plural = 'Campeões'
+
+    def __str__(self):
+        return self.nome
