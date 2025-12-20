de0964fb8ce4e7f64891061fa57dadecbef7c964
Lostyzz <lucaslostyz@gmail.com>
2025-12-19 23:52:53 -0300

    Lista de campeões na pagina inicial

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Campeao
+# Create your views here.
+
 
 def home(request):
-    return render(request, 'core/home.html')
+    campeoes = Campeao.objects.all()
+    return render(request, 'core/home.html', {
+        'campeoes': campeoes
+    })
