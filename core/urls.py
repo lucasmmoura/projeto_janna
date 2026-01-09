1c69da4d14d7799b8f0e03bfa0567c20a057c333
Lostyzz <lucaslostyz@gmail.com>
2026-01-09 10:36:07 -0300

    Cria pagian de detalhe de campeao

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, camepao_detalhe
 
 urlpatterns = [
     path('', home, name='home'),
+    path('campeao/<int:id>/', camepao_detalhe, name='campeao_detalhe'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Campeao
 # Create your views here.
@@ -8,4 +8,11 @@ def home(request):
     campeoes = Campeao.objects.all()
     return render(request, 'core/home.html', {
         'campeoes': campeoes
+    })
+
+def campeao_detalhe(request, id):
+    campeao = get_object_or_404(Campeao, id=id) #Busca no banco um campeão com o id igual ao da url; Se achar retorna o objeto, se não achar = pagina 404
+    return render(request, "core/campeao_detalhe_html", # Template
+                  {
+        "campeao": campeao #Objeto para o template
     })
